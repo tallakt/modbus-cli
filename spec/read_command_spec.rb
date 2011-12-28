34e2be28ddf94de104427afb9312195bc52e4cb8
Ebon Elza <ebonelza@gmail.com>
2011-12-28 16:34:04 -0500

    Modify read tests to include TCP port

--- spec/read_command_spec.rb
+++ spec/read_command_spec.rb
@@ -11,7 +11,7 @@ describe Modbus::Cli::ReadCommand do
   end
 
   it 'can read registers' do
-    client, slave = standard_connect_helper '1.2.3.4'
+    client, slave = standard_connect_helper '1.2.3.4', 502
     slave.should_receive(:read_holding_registers).with(100, 10).and_return((0..9).to_a)
     cmd.run %w(read 1.2.3.4 %MW100 10)
     stdout.should match(/^\s*%MW105\s*5$/)
@@ -20,28 +20,28 @@ describe Modbus::Cli::ReadCommand do
 
 
   it 'can read floating point numbers' do
-    client, slave = standard_connect_helper '1.2.3.4'
+    client, slave = standard_connect_helper '1.2.3.4', 502
     slave.should_receive(:read_holding_registers).with(100, 4).and_return([52429, 17095, 52429, 17095])
     cmd.run %w(read 1.2.3.4 %MF100 2)
     stdout.should match(/^\s*%MF102\s*99[.]9(00[0-9]*)?$/)
   end
 
   it 'should display numbers with good precision' do
-    client, slave = standard_connect_helper '1.2.3.4'
+    client, slave = standard_connect_helper '1.2.3.4', 502
     slave.should_receive(:read_holding_registers).and_return([1049, 16286])
     cmd.run %w(read 1.2.3.4 %MF100 1)
     stdout.should match(/^\s*%MF100\s*1[.]2345\d*$/)
   end
 
   it 'can read double word numbers' do
-    client, slave = standard_connect_helper '1.2.3.4'
+    client, slave = standard_connect_helper '1.2.3.4', 502
     slave.should_receive(:read_holding_registers).with(100, 4).and_return([16959, 15, 16959, 15])
     cmd.run %w(read 1.2.3.4 %MD100 2)
     stdout.should match(/^\s*%MD102\s*999999$/)
   end
 
   it 'can read coils' do
-    client, slave = standard_connect_helper '1.2.3.4'
+    client, slave = standard_connect_helper '1.2.3.4', 502
     slave.should_receive(:read_coils).with(100, 10).and_return([1, 0] * 5)
     cmd.run %w(read 1.2.3.4 %M100 10)
     stdout.should match(/^\s*%M105\s*0$/)
@@ -61,53 +61,53 @@ describe Modbus::Cli::ReadCommand do
   end
 
   it 'should split large reads into smaller chunks for words' do
-    client, slave = standard_connect_helper '1.2.3.4'
+    client, slave = standard_connect_helper '1.2.3.4', 502
     slave.should_receive(:read_holding_registers).with(100, 125).and_return([1, 0] * 1000)
     slave.should_receive(:read_holding_registers).with(225, 25).and_return([1, 0] * 1000)
     cmd.run %w(read 1.2.3.4 %MW100 150)
   end
 
   it 'should split large reads into smaller chunks for coils' do
-    client, slave = standard_connect_helper '1.2.3.4'
+    client, slave = standard_connect_helper '1.2.3.4', 502
     slave.should_receive(:read_coils).with(100, 2000).and_return([1, 0] * 1000)
     slave.should_receive(:read_coils).with(2100, 1000).and_return([1, 0] * 500)
     cmd.run %w(read 1.2.3.4 %M100 3000)
   end
 
   it 'can read registers as ints' do
-    client, slave = standard_connect_helper '1.2.3.4'
+    client, slave = standard_connect_helper '1.2.3.4', 502
     slave.should_receive(:read_holding_registers).with(100, 1).and_return([0xffff])
     cmd.run %w(read --int 1.2.3.4 %MW100 1)
     stdout.should match(/^\s*%MW100\s*-1$/)
   end
 
   it 'can read registers as floats' do
-    client, slave = standard_connect_helper '1.2.3.4'
+    client, slave = standard_connect_helper '1.2.3.4', 502
     slave.should_receive(:read_holding_registers).with(100, 2).and_return([0,0])
     cmd.run %w(read --float 1.2.3.4 %MW100 1)
   end
 
   it 'can read registers as dwords' do
-    client, slave = standard_connect_helper '1.2.3.4'
+    client, slave = standard_connect_helper '1.2.3.4', 502
     slave.should_receive(:read_holding_registers).with(100, 2).and_return([0,0])
     cmd.run %w(read --dword 1.2.3.4 %MW100 1)
   end
 
   it 'can read registers as words' do
-    client, slave = standard_connect_helper '1.2.3.4'
+    client, slave = standard_connect_helper '1.2.3.4', 502
     slave.should_receive(:read_holding_registers).with(100, 1).and_return([0])
     cmd.run %w(read --word 1.2.3.4 %MD100 1)
   end
 
   it 'accepts Modicon addresses for coils' do
-    client, slave = standard_connect_helper '1.2.3.4'
+    client, slave = standard_connect_helper '1.2.3.4', 502
     slave.should_receive(:read_coils).with(100, 10).and_return([1, 0] * 5)
     cmd.run %w(read 1.2.3.4 101 10)
     stdout.should match(/^\s*106\s*0$/)
   end
 
   it 'accepts Modicon addresses for registers' do
-    client, slave = standard_connect_helper '1.2.3.4'
+    client, slave = standard_connect_helper '1.2.3.4', 502
     slave.should_receive(:read_holding_registers).with(100, 10).and_return((0..9).to_a)
     cmd.run %w(read 1.2.3.4 400101 10)
     stdout.should match(/^\s*400106\s*5$/)
@@ -115,14 +115,14 @@ describe Modbus::Cli::ReadCommand do
 
 
   it 'should accept the --modicon option to force modicon output' do
-    client, slave = standard_connect_helper '1.2.3.4'
+    client, slave = standard_connect_helper '1.2.3.4', 502
     slave.should_receive(:read_holding_registers).with(100, 10).and_return((0..9).to_a)
     cmd.run %w(read --modicon 1.2.3.4 %MW100 10)
     stdout.should match(/^\s*400106\s*5$/)
   end
 
   it 'should accept the --schneider option to force schneider output' do 
-    client, slave = standard_connect_helper '1.2.3.4'
+    client, slave = standard_connect_helper '1.2.3.4', 502
     slave.should_receive(:read_holding_registers).with(100, 10).and_return((0..9).to_a)
     cmd.run %w(read --schneider 1.2.3.4 400101 10)
     stdout.should match(/^\s*%MW105\s*5$/)
@@ -130,27 +130,27 @@ describe Modbus::Cli::ReadCommand do
 
   it 'has a --slave parameter' do
     client = mock 'client'
-    ModBus::TCPClient.should_receive(:connect).with('X').and_yield(client)
+    ModBus::TCPClient.should_receive(:connect).with('X', 502).and_yield(client)
     client.should_receive(:with_slave).with(99)
     cmd.run %w(read --slave 99 X 101 1)
   end
 
   it 'can write the output from reading registers to a yaml file using the -o <filename> parameter' do
-    client, slave = standard_connect_helper '1.2.3.4'
+    client, slave = standard_connect_helper '1.2.3.4', 502
     slave.should_receive(:read_holding_registers).with(100, 1).and_return([1])
     file_mock = mock('file')
     File.should_receive(:open).and_yield(file_mock)
-    file_mock.should_receive(:puts).with({:host => '1.2.3.4', :slave => 1, :offset => '400101', :data => [1]}.to_yaml)
+    file_mock.should_receive(:puts).with({:host => '1.2.3.4', :port => 502, :slave => 1, :offset => '400101', :data => [1]}.to_yaml)
     cmd.run %w(read --output filename.yml 1.2.3.4 %MW100 1)
     stdout.should_not match(/./)
   end
 
   it 'can write the output from reading coils to a yaml file using the -o <filename> parameter' do
-    client, slave = standard_connect_helper '1.2.3.4'
+    client, slave = standard_connect_helper '1.2.3.4', 502
     slave.should_receive(:read_coils).with(100, 1).and_return([1])
     file_mock = mock('file')
     File.should_receive(:open).and_yield(file_mock)
-    file_mock.should_receive(:puts).with({:host => '1.2.3.4', :slave => 1, :offset => '101', :data => [1]}.to_yaml)
+    file_mock.should_receive(:puts).with({:host => '1.2.3.4', :port => 502, :slave => 1, :offset => '101', :data => [1]}.to_yaml)
     cmd.run %w(read --output filename.yml 1.2.3.4 %M100 1)
     stdout.should_not match(/./)
   end
